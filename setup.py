ef8e533469e8daf1ce38b32db19578d2ef1648ce
Aniket Shelar <aniket.shelar@infobeans.com>
2025-06-26 18:30:01 +0530

    Adding the setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rules_pricing_engine',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Python library that provides the rules and pricing for deriving a premium for an insurance.',
+    author='Me',
+)
